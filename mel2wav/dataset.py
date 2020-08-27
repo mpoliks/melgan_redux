2a945b1f86414f0e4fb44fa2df1c04ef5041ed63
sveitser <sveitser@gmail.com>
2020-08-27 19:04:59 +0800

    Dataset: sample from audiofiles weighted by length

--- mel2wav/dataset.py
+++ mel2wav/dataset.py
@@ -2,12 +2,25 @@ import torch
 import torch.utils.data
 import torch.nn.functional as F
 
+import librosa
 from librosa.core import load
 from librosa.util import normalize
 
 from pathlib import Path
+from dataclasses import dataclass
 import numpy as np
 import random
+
+
+@dataclass
+class AudioFile:
+    path: str
+    duration: float
+
+    @classmethod
+    def from_path(cls, path):
+        duration = librosa.get_duration(filename=path)
+        return cls(path, duration)
 
 
 def files_to_list(filename):
@@ -30,16 +43,21 @@ class AudioDataset(torch.utils.data.Dataset):
     def __init__(self, training_files, segment_length, sampling_rate, augment=True):
         self.sampling_rate = sampling_rate
         self.segment_length = segment_length
-        self.audio_files = files_to_list(training_files)
-        self.audio_files = [Path(training_files).parent / x for x in self.audio_files]
+        files = files_to_list(training_files)
+        files = [Path(training_files).parent / x for x in files]
+        self.audio_files = [AudioFile.from_path(path) for path in files]
+        self.durations = np.array([a.duration for a in self.audio_files])
+        self.duration = self.durations.sum()
+        self.weights = self.durations / self.duration
         random.seed(1234)
-        random.shuffle(self.audio_files)
         self.augment = augment
 
     def __getitem__(self, index):
-        # Read audio
-        filename = self.audio_files[index]
-        audio, sampling_rate = self.load_wav_to_torch(filename)
+
+        audio_file = np.random.choice(self.audio_files, p=self.weights)
+        offset = np.random.randint(audio_file.duration)
+        audio = self.load_wav_to_torch(audio_file.path, offset)
+
         # Take segment
         if audio.size(0) >= self.segment_length:
             max_audio_start = audio.size(0) - self.segment_length
@@ -54,17 +72,20 @@ class AudioDataset(torch.utils.data.Dataset):
         return audio.unsqueeze(0)
 
     def __len__(self):
-        return len(self.audio_files)
+        return int(self.durations.sum() * self.sampling_rate / self.segment_length)
 
-    def load_wav_to_torch(self, full_path):
+    def load_wav_to_torch(self, full_path, offset):
         """
         Loads wavdata into torch array
         """
-        data, sampling_rate = load(full_path, sr=self.sampling_rate)
+        load_duration = 2 * self.segment_length / self.sampling_rate
+        data, _ = load(
+            full_path, sr=self.sampling_rate, offset=offset, duration=load_duration
+        )
         data = 0.95 * normalize(data)
 
         if self.augment:
             amplitude = np.random.uniform(low=0.3, high=1.0)
             data = data * amplitude
 
-        return torch.from_numpy(data).float(), sampling_rate
+        return torch.from_numpy(data).float()
