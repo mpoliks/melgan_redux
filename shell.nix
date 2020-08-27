c197f64da9b81223e19eb9168788d5e510b47227
sveitser <sveitser@gmail.com>
2020-08-27 19:09:03 +0800

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,59 @@
+with import ./nix/nixpkgs.nix {
+
+  overlays = [
+
+    (self: super:
+      {
+        blas = super.blas.override {
+          blasProvider = self.mkl;
+        };
+        lapack = super.lapack.override {
+          lapackProvider = self.mkl;
+        };
+      }
+    )
+  ];
+
+};
+
+let
+  py = python3;
+in
+mkShell {
+  buildInputs = [
+
+    ffmpeg
+    # ffmpeg-full
+    entr
+
+    (py.withPackages (ps: with ps; [
+
+      librosa
+      # ffmpeg-full leads to assertion error with blas override.
+      (pydub.override { ffmpeg-full = ffmpeg; })
+      click
+      pyyaml
+      pytorchWithCuda
+      scipy
+
+      # To install packages not in nixpkgs
+      pip
+
+      # dev deps
+      pudb  # debugger
+      black
+      ipython
+      pyls-isort
+      pyls-black
+      pyls-mypy
+      python-language-server
+    ]))
+   ];
+
+  shellHook = ''
+    export PIP_PREFIX="$(pwd)/.build/pip_packages"
+    export PATH="$PIP_PREFIX/bin:$PATH"
+    export PYTHONPATH="$PIP_PREFIX/${py.sitePackages}:$PYTHONPATH"
+    unset SOURCE_DATE_EPOCH
+  '';
+}
