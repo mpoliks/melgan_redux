95ed136189d8e78fce8c07700f6ea7e7293b1ce8
Ryan Au <ryau123@gmail.com>
2021-06-02 21:56:32 +0800

    modify dataset to use left channel instead of combine channels

--- mel2wav/dataset.py
+++ mel2wav/dataset.py
@@ -77,8 +77,14 @@ class AudioDataset(torch.utils.data.Dataset):
         """
         load_duration = self.segment_length / self.sampling_rate
         data, _ = load(
-            full_path, sr=self.sampling_rate, offset=offset, duration=load_duration
+            full_path,
+            sr=self.sampling_rate,
+            offset=offset,
+            duration=load_duration,
+            mono=False,
         )
+        if len(data.shape) > 1:
+            data = data[0]
         # data = 0.95 * normalize(data)
 
         # if self.augment:
